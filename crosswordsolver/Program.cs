74311a2dd8390198219ad0ae8a714fa5422a0227
Richard L. Taylor Jr <rtaylor@rightincode.com>
2017-06-21 16:49:33 -0400

    Final changes

--- crosswordsolver/Program.cs
+++ crosswordsolver/Program.cs
@@ -8,8 +8,47 @@ namespace crosswordsolver
 {
     class Program
     {
+        static string inputString;
+        static int inputLength;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Initializing");
+            WordDictionary words = new WordDictionary();
+            Console.WriteLine("Initializaiton Complete");
+
+            do {
+
+                PromptUser();
+
+                if (inputLength == 0 || inputString.Length == 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    Console.WriteLine("Hit the enter key to search again or 'Q' to quit");
+                    continue;
+                }
+
+                List<string> matchedWords = words.FindMatch(inputLength, inputString);
+
+                foreach (string matchedWord in matchedWords)
+                {
+                    Console.WriteLine(matchedWord);
+                }
+                Console.WriteLine("Search complete. Elapsed ticks for search: {0}", words.elapsedTicks);
+                Console.WriteLine("Hit the enter key to search again or 'Q' to quit");
+
+            } while (Console.ReadLine().ToUpper() != "Q");            
+        }
+
+        static void PromptUser()
+        {
+            Console.WriteLine("Welcome to the crossword solver.");
+            Console.Write("Please enter the length (numerical value) of the word(s) you are searching: ");
+            int.TryParse(Console.ReadLine(), out inputLength);
+
+            Console.WriteLine("You are searching for word(s) with a length of {0}", inputLength);
+            Console.WriteLine("Please enter the word pattern. Use '.' for matching 'any' letter in the specified position. (i.e. '.A.CE'");
+            inputString = Console.ReadLine();
         }
     }
 }
---
+++ crosswordsolver/WordDictionary.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace crosswordsolver
+{
+    public class WordDictionary
+    {
+        private Dictionary<int, string> _wordDictionary;
+        private long _elapsedTicks;
+        
+        public Dictionary<int, string> wordDictionary
+        {
+            get
+            {
+                return _wordDictionary;
+            }
+        }
+
+        public long elapsedTicks
+        {
+            get { return _elapsedTicks; }
+        }
+
+        public WordDictionary()
+        {            
+            LoadDictionary();            
+        }
+
+        public List<string> FindMatch(int length, string inputString)
+        {
+            List<string> matchedWordsList = new List<string>();
+            Stopwatch stopwatch = new Stopwatch();
+
+            Regex searchExp = new Regex(BuildRegexPattern(inputString, length));
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            var matchedWordsByLength = _wordDictionary[length];
+            MatchCollection matchedWords = searchExp.Matches(matchedWordsByLength);
+
+            stopwatch.Stop();
+            _elapsedTicks = stopwatch.ElapsedTicks;
+            
+            foreach (Match match in matchedWords)
+            {
+                matchedWordsList.Add(match.Value);
+            }
+
+            return matchedWordsList;            ;
+        }
+
+        private void LoadDictionary()
+        {
+            _wordDictionary = new Dictionary<int, string>();
+
+            using(StreamReader wordFile = new StreamReader("dictionary.txt"))
+            {
+                while((!wordFile.EndOfStream))
+                {
+                    var word = wordFile.ReadLine();
+                    if (word.Length > 0)
+                    {
+                        int wordLength = word.Length;
+                        if (!_wordDictionary.ContainsKey(wordLength))
+                        {
+                            _wordDictionary.Add(wordLength, "");
+                        }
+
+                        _wordDictionary[wordLength] = _wordDictionary[wordLength] + word + " ";
+                    }
+                    
+                }
+            }
+
+            FindMatch(5, ".A..E");      //test
+        }
+
+        private string BuildRegexPattern(string inputString, int patternLength)
+        {
+            string regexPattern = "";
+            
+            if (inputString.Length > patternLength)
+            {
+                inputString = inputString.Substring(0, patternLength);
+            }
+
+            StringBuilder sb = new StringBuilder(inputString);
+            for (var i = inputString.Length; i < patternLength; i++)
+            {
+                sb.Append(".");
+            }
+
+            regexPattern = sb.ToString().Replace(".", "[A-Z]").ToUpper();
+
+            return regexPattern;
+        }
+    }
+}
